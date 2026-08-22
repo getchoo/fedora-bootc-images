457214690ba997477fb51dfdc1b03bb8d92e3ec1
Seth Flynn <getchoo@tuta.io>
2026-08-22 03:37:40 -0400

    use base fedora image for builder

--- scripts/build-nvidia-kmod.sh
+++ scripts/build-nvidia-kmod.sh
@@ -2,10 +2,7 @@
 source "$(readlink -f "$0" | xargs dirname)"/helpers.sh
 
 setup-rpmfusion
-# FIXME: Hide our ostree-ness from kmodtool so scriptlets don't fail
-# https://bugzilla.redhat.com/show_bug.cgi?id=2459819
-sed -i "/^OSTREE_VERSION='/d" /etc/os-release
-dnf-minimal-install akmod-nvidia
+dnf-minimal-install akmod-nvidia kernel{,-devel}
 
 setup-kmod-signing
 build-kmod nvidia
